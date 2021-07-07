00e346449405881917a0c6a8d537df2162807690
aidan <BLYAID001@myuct.ac.za>
2021-07-07 21:52:25 +0200

    Added executable

---
+++ app/Main.hs
@@ -0,0 +1,8 @@
+module Main where
+
+import           DefeasibleLogic
+import           DefeasibleTypes
+import           PropositionalLogic
+import           PropositionalTypes
+
+main = print "Welcome to HasKLM!"
