b25e708ee33fcdb859dca17e2027112c28b255e6
aidan <BLYAID001@myuct.ac.za>
2021-07-08 10:16:36 +0200

    Added leaky boat test

--- test/MyLibTest.hs
+++ test/MyLibTest.hs
@@ -8,13 +8,14 @@ import           PropositionalTypes
 main :: IO ()
 main = do
   quickCheck parserTest
-  quickCheck rationalClosureTest
+  quickCheck penguinRCTest
+  quickCheck leakyboatRCTest
 
 parserTest = do
   let form = str2form "A And B"
   form == And (Atom "A") (Atom "B")
 
-rationalClosureTest = do
+penguinRCTest = do
   let dkb =
         [ typically (str2form "b Implies f")
         , typically (str2form "p Implies Not f")
@@ -23,4 +24,17 @@ rationalClosureTest = do
   let kb = [str2form "p Implies b", str2form "R Implies b"]
   let jkb   = (kb, dkb)
   let query = typically (str2form "p Implies b")
+  entailsRC jkb query
+
+leakyboatRCTest = do
+  let dkb =
+        [ typically (str2form "boat Implies floats")
+        , typically (str2form "leaky Implies boat")
+        , typically (str2form "leaky Implies Not floats")
+        , typically (str2form "FlyingDutchman Implies boat")
+        , typically (str2form "FlyingDutchman Implies leaky")
+        ]
+  let kb    = []
+  let jkb   = (kb, dkb)
+  let query = typically (str2form "FlyingDutchman Implies Not floats")
   entailsRC jkb query
